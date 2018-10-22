9f88113c5ed7d71fc6ab9eb4bb25f4b19856b30b
Lauren Piontek <piontekle@gmail.com>
2018-10-22 19:22:51 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,3 +1,5 @@
+#display board accepts input
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -5,14 +7,30 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
-
-# code your input_to_index and move method here!
-
+#converts user input to index number
 def input_to_index(input)
   index = input.to_i - 1
 end
 
 def move(array, index, player = "X")
   array[index] = player
+end
+
+#checks if valid move
+def valid_move?(board, index)
+  if !position_taken?(board, index) && index.between?(0,8)
+    return true
+  else
+    return false
+  end
+end
+
+#checks if position already taken
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] ==  nil
+    return false
+    elsif board[index] == "X" || "O"
+    return true
+  end
 end
 
